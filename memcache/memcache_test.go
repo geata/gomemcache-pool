abf0cffa0f4c1cdc55dc22c71ed22bb00ffbc96e
geata <geatay@gmail.com>
2023-12-18 22:43:20 +0800

    add pool for test

--- memcache/memcache_test.go
+++ memcache/memcache_test.go
@@ -413,6 +413,7 @@ func BenchmarkOnItem(b *testing.B) {
 
 	addr := fakeServer.Addr()
 	c := New(addr.String())
+	c.PreparePool()
 	if _, err := c.getConn(addr); err != nil {
 		b.Fatal("failed to initialize connection to fake server")
 	}
